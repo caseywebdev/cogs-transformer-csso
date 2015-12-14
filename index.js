42968523d19f00479de50c96662f35c35933e0cc
Casey Foster <c@sey.me>
2015-12-14 16:41:23 -0600

    csso@1.4.4, 1.0.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var csso = require('csso');
 
 module.exports = function (file, options, cb) {
   var source = file.buffer.toString();
-  try { source = csso.justDoIt(source) + '\n'; }
+  try { source = csso.minify(source) + '\n'; }
   catch (er) { return cb(er); }
   cb(null, {buffer: new Buffer(source)});
 };
