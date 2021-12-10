1decd358d0bbc5e4eb99c3163d13c2143f82edbd
Casey Foster <c@sey.me>
2021-12-09 21:19:42 -0600

    Update csso

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import csso from 'csso';
+import { minify } from 'csso';
 
 export default ({ file, options }) => {
-  const { css } = csso.minify(file.buffer, { filename: file.path, ...options });
+  const { css } = minify(file.buffer.toString(), {
+    filename: file.path,
+    ...options
+  });
   return { buffer: Buffer.from(`${css}\n`) };
 };
